077e399bb1e9a6387f826cbeb0e09604b5a787b5
yulyanes <yoeker@gmail.com>
2020-10-07 12:32:28 +0700

    first commit

---
+++ App.js
@@ -0,0 +1,42 @@
+new Vue({
+    el: '#app',
+    data: {
+        title: 'Aplikasi Kasir',
+        namaKasir: '',
+        kodeBarang: '',
+        hargaBarang: 0,
+        jumlahBeli: 0,
+        totalHarga: 0,
+        tampilkanStruk: false
+    },
+    methods: {
+        buatStruk() {
+            this.totalHarga = this.hargaBarang * this.jumlahBeli;
+            this.tampilkanStruk = true;
+        },
+
+        bersihkanData() {
+            this.namaKasir = '';
+            this.kodeBarang = '';
+            this.hargaBarang = '';
+            this.jumlahBeli = '';
+            this.totalHarga = '';
+            this.tampilkanStruk = false;
+        }
+    },
+    computed: {
+        hargaBarangFormatted() {
+            let rupiah = '';
+            let angkaRev = this.totalHarga.toString().split('').reverse().join('');
+            for (var i = 0; i < angkaRev.length; i++) if (i % 3 == 0) rupiah += angkaRev.substr(i, 3) + '.';
+            return 'Rp. ' + rupiah.split('', rupiah.length - 1).reverse().join('');
+        },
+
+        totalHargaFormatted() {
+            let rupiah = '';
+            let angkaRev = this.totalHarga.toString().split('').reverse().join('');
+            for (var i = 0; i < angkaRev.length; i++) if (i % 3 == 0) rupiah += angkaRev.substr(i, 3) + '.';
+            return 'Rp. ' + rupiah.split('', rupiah.length - 1).reverse().join('');
+        }
+    }
+});
